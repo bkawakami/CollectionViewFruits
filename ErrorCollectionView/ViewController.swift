dc066314d8fa99b016d10eb3b65b2b0c8a3f211f
Bruno Kawakami <bru.kawakami@gmail.com>
2015-12-06 12:11:12 -0200

    Initial Commit

---
+++ ErrorCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ErrorCollectionView
+//
+//  Created by Bruno Kawakami on 12/6/15.
+//  Copyright © 2015 Bruno Kawakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
